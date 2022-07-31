7de7c26d322a45a2d76f57c05d28294069248a8a
lizebin <lizebin0918@gmail.com>
2022-07-31 23:28:39 +0800

    refactor

--- src/main/java/com/lzb/di/Context.java
+++ src/main/java/com/lzb/di/Context.java
@@ -1,5 +1,7 @@
 package com.lzb.di;
 
+import jakarta.inject.Provider;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,19 +13,20 @@ import java.util.Map;
  */
 public class Context {
 
-    private Map<Class<?>, Object> components = new HashMap<>();
-    private Map<Class<?>, Class<?>> componentImplementations = new HashMap<>();
+    private Map<Class<?>, Provider<?>> providers = new HashMap<>();
 
-    public <ComponentType> void bind(Class<ComponentType> type, ComponentType instance) {
-        components.put(type, instance);
+    public <T> void bind(Class<T> type, T instance) {
+        // 还能这样转
+        providers.put(type, (Provider<T>) () -> instance);
     }
 
-    public <ComponentType> ComponentType get(Class<ComponentType> type) {
-        if (components.containsKey(type)) {
-            return (ComponentType) components.get(type);
-        }
+    public <T> T get(Class<T> type) {
+        return (T) providers.get(type).get();
+    }
+
+    private <T> T getComponentType(Class<?> implementation) {
         try {
-            return (ComponentType) componentImplementations.get(type).getConstructor().newInstance();
+            return (T) implementation.getConstructor().newInstance();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -31,6 +34,6 @@ public class Context {
 
     public <ComponentType, ComponentTypeImplementation extends ComponentType> void bind(Class<ComponentType> componentType,
                                                                                         Class<ComponentTypeImplementation> implementation) {
-        componentImplementations.put(componentType, implementation);
+        providers.put(componentType, (Provider<ComponentType>) () -> getComponentType(implementation));
     }
 }
