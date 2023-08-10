4b988f30c08828898f28557111a692e69e590e65
lizebin <lizebin0918@gmail.com>
2023-08-10 12:00:46 +0800

    feat:FizzBuzzWhizz题目

---
+++ src/test/java/com/lzb/fizz_buzz_whizz/FizzBuzzWhizzUnitTest.java
@@ -0,0 +1,32 @@
+package com.lzb.fizz_buzz_whizz;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * https://www.bilibili.com/video/BV1xs411X7iv/?spm_id_from=333.788.recommend_more_video.11&vd_source=a8ffd7af1e5eb1765c32f637267c1ef0
+ * 题目描述<br/>
+ * rule1
+ * - times(3) -> Fizz
+ * - times(5) -> Buzz
+ * - times(7) -> Whizz
+ * rule2
+ * - times(3) &amp;&amp; times(5) &amp;&amp; times(7) -> FizzBuzzWhizz
+ * - times(3) &amp;&amp; times(5) -> FizzBuzz
+ * - times(3) &amp;&amp; times(7) -> FizzWhizz
+ * - times(5) &amp;&amp; times(7) -> BuzzWhizz
+ * rule3
+ * - contains(3) -> Fizz
+ * - the priority of contains(3) is highest
+ * rd
+ * - others -> others
+ * Created on : 2023-08-10 11:38
+ * @author mac
+ */
+class FizzBuzzWhizzUnitTest {
+
+    @Test
+    void test() {
+
+    }
+
+}
