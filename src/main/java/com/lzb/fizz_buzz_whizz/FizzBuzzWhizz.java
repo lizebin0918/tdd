d6c74f89323d0fdfa41a997a48c73f34db3960df
lizebin <lizebin0918@gmail.com>
2023-08-11 17:01:12 +0800

    feat:单个倍数判断

---
+++ src/main/java/com/lzb/fizz_buzz_whizz/FizzBuzzWhizz.java
@@ -0,0 +1,27 @@
+package com.lzb.fizz_buzz_whizz;
+
+import java.util.Objects;
+
+/**
+ * <br/>
+ * Created on : 2023-08-11 11:22
+ * @author lizebin
+ */
+public record FizzBuzzWhizz(int fizzNum, int buzzNum, int whizzNum) {
+
+    String print(int input) {
+        return Objects.toString(input);
+    }
+
+    boolean isWhizzTimes(int input) {
+        return input % whizzNum == 0;
+    }
+
+    boolean isFizzTimes(int input) {
+        return input % fizzNum == 0;
+    }
+
+    boolean isBuzzTimes(int input) {
+        return input % buzzNum == 0;
+    }
+}
--- src/test/java/com/lzb/fizz_buzz_whizz/FizzBuzzWhizzUnitTest.java
+++ src/test/java/com/lzb/fizz_buzz_whizz/FizzBuzzWhizzUnitTest.java
@@ -1,6 +1,9 @@
 package com.lzb.fizz_buzz_whizz;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * https://www.bilibili.com/video/BV1xs411X7iv/?spm_id_from=333.788.recommend_more_video.11&vd_source=a8ffd7af1e5eb1765c32f637267c1ef0
@@ -24,9 +27,52 @@ import org.junit.jupiter.api.Test;
  */
 class FizzBuzzWhizzUnitTest {
 
-    @Test
-    void test() {
+    private FizzBuzzWhizz fizzBuzzWhizz;
 
+    @BeforeEach
+    void setUp() {
+        fizzBuzzWhizz = new FizzBuzzWhizz(1, 2, 3);
+    }
+
+    @Test
+    void should_instance_FizzBuzzWhizz() {
+        assertEquals(1, fizzBuzzWhizz.fizzNum());
+        assertEquals(2, fizzBuzzWhizz.buzzNum());
+        assertEquals(3, fizzBuzzWhizz.whizzNum());
+    }
+
+    @Test
+    void should_print() {
+        // given
+        int i = 3;
+
+        // when
+        String r = fizzBuzzWhizz.print(i);
+
+        // then
+        assertEquals("3", r);
+    }
+
+    @Test
+    void should_print_Whizz() {
+        assertTrue(fizzBuzzWhizz.isWhizzTimes(3));
+        assertFalse(fizzBuzzWhizz.isWhizzTimes(4));
+        assertFalse(fizzBuzzWhizz.isWhizzTimes(5));
+    }
+
+    @Test
+    void should_print_Fizz() {
+        assertTrue(fizzBuzzWhizz.isFizzTimes(1));
+        assertTrue(fizzBuzzWhizz.isFizzTimes(2));
+        assertTrue(fizzBuzzWhizz.isFizzTimes(3));
+    }
+
+    @Test
+    void should_print_Buzz() {
+        assertFalse(fizzBuzzWhizz.isBuzzTimes(1));
+        assertTrue(fizzBuzzWhizz.isBuzzTimes(2));
+        assertFalse(fizzBuzzWhizz.isBuzzTimes(3));
+        assertTrue(fizzBuzzWhizz.isBuzzTimes(4));
     }
 
 }
