77d76aac68a12d5c3e4c8529922c515804b439a6
lizebin <lizebin0918@gmail.com>
2023-10-02 17:50:47 +0800

    feat:改成Provider方式

--- src/test/java/com/lzb/container/Context.java
+++ src/test/java/com/lzb/container/Context.java
@@ -5,6 +5,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 
+import jakarta.inject.Provider;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,23 +17,23 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Context {
 
-    private static final Map<Class<?>, Object> components = new HashMap<>();
-    private static final Map<Class<?>, Class<?>> componentImpls = new HashMap<>();
+    private static final Map<Class<?>, Provider<?>> newComponents = new HashMap<>();
 
     <T> void bind(Class<T> componentClass, T instance) {
-        components.put(componentClass, instance);
+        newComponents.put(componentClass, () -> instance);
     }
 
-    @SneakyThrows
     <T> T get(Class<T> componentClass) {
-        T t = (T) components.get(componentClass);
-        if (null != t) {
-            return t;
-        }
-        return (T) componentImpls.get(componentClass).getConstructors()[0].newInstance();
+        return (T) newComponents.get(componentClass).get();
     }
 
     <Component, ComponentImpl extends Component> void bind(Class<Component> componentClass, Class<ComponentImpl> implementationClass) {
-        componentImpls.put(componentClass, implementationClass);
+        newComponents.put(componentClass, () -> {
+            try {
+                return implementationClass.getConstructors()[0].newInstance();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
