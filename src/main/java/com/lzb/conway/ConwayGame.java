83c02640392831d12ce15294a1f189bd064b5dff
lizebin <lizebin0918@gmail.com>
2024-02-15 23:16:36 +0800

    revert:还原最初始化状态

--- src/main/java/com/lzb/conway/ConwayGame.java
+++ src/main/java/com/lzb/conway/ConwayGame.java
@@ -12,31 +12,4 @@ import lombok.Getter;
 @Getter
 public class ConwayGame {
 
-    private final Cells cells;
-
-    ConwayGame(int x, int y) {
-        cells = new Cells(x, y);
-    }
-
-    ConwayGame(int[][] grid) {
-        cells = new Cells(grid);
-    }
-
-    int[][] getGrid() {
-        return cells.getGrid();
-    }
-
-    void tick() {
-        for (int i = 0; i < cells.getX(); i++) {
-            for (int j = 0; j < cells.getY(); j++) {
-                int count = cells.liveCountFrom(i, j);
-                if (cells.isDead(i, j) && count == 3) {
-                    cells.live(i, j);
-                }
-                if (cells.isLive(i, j) && (count < 2 || count > 3)) {
-                    cells.dead(i, j);
-                }
-            }
-        }
-    }
 }
