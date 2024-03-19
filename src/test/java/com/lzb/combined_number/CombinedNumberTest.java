56366e53402c82c578eb1fc3235a7bc63508b9ac
lizebin <lizebin0918@gmail.com>
2024-03-19 08:51:10 +0800

    revert

--- src/test/java/com/lzb/combined_number/CombinedNumberTest.java
+++ src/test/java/com/lzb/combined_number/CombinedNumberTest.java
@@ -19,6 +19,68 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 class CombinedNumberTest {
 
+    @Test
+    @DisplayName("找出最左边的数字最大的那个数，比如：[100, 2, 3]，返回索引 index")
+    void should_return_2_when_input_100_2_3_start_index_0() {
+        assertThat(CombinedNumber.maxNumOfLeftMost(new int[]{100, 2, 3}, 0, 0, 3)).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("找出最左边的数字最大的那个数，比如：[100, 2, 3, 4]，返回索引 index")
+    void should_return_3_when_input_100_2_3_4_start_index_0() {
+        assertThat(CombinedNumber.maxNumOfLeftMost(new int[]{100, 2, 3, 4}, 0, 0, 3)).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("找出最左边的数字最大的那个数，比如：[100, 2, 3, 4]，返回索引 3")
+    void should_return_3_when_input_100_2_3_4_start_index_1() {
+        assertThat(CombinedNumber.maxNumOfLeftMost(new int[]{100, 2, 3, 4}, 1, 0, 3)).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("swap")
+    void should_swap() {
+        int[] inputs = {100, 2, 3};
+        CombinedNumber.swap(inputs, 0, 1);
+        assertThat(inputs).isEqualTo(new int[]{2, 100, 3});
+    }
+
+    @Test
+    @DisplayName("combine: 100, 2, 3 -> 10023")
+    void should_return_10023_when_input_100_2_3() {
+        assertThat(CombinedNumber.combine(new int[]{100, 2, 3})).isEqualTo("32100");
+    }
+
+    @Test
+    @DisplayName("maxNumOfLeftMost([32, 33, 34], 0) -> 2")
+    void should_return_2_when_maxNumOfLeftMost_32_33_34_0() {
+        assertThat(CombinedNumber.maxNumOfLeftMost(new int[]{32, 33, 34}, 0, 1, 2)).isEqualTo(2);
+    }
+
+
+
+    @Nested
+    class GetNumCharAt {
+        @Test
+        @DisplayName("getNumCharAt([32, 33, 345], 0, 2) -> 32, 33, 345")
+        void should_return_2_when_getNumCharAt_32_33_345_0_2() {
+            assertThat(CombinedNumber.getNumCharAt(new int[]{32, 33, 345}, 0, 2, 3)).isEqualTo(new int[]{2, 3, 5});
+        }
+
+        @Test
+        @DisplayName("获取整个数组的最左边的数，100_2_3_4 返回数组 100_2_3_4")
+        void should_return_array_of_left_most_when_input_100_2_3_4_start_index_1() {
+            assertThat(CombinedNumber.getNumCharAt(new int[]{100, 2, 3, 4}, 1, 0. )).isEqualTo(new int[]{100, 2, 3, 4});
+        }
+
+        /*
+        @Test
+        @DisplayName("获取整个数组的最左边的数，100_2_3_4 返回数组 1_2_3_4")
+        void should_return_array_of_left_most_when_input_100_2_3_4_start_index_0() {
+            assertThat(CombinedNumber.getNumCharAt(new int[]{100, 2, 3, 4}, 0, 0)).isEqualTo(new int[]{1, 2, 3, 4});
+        }*/
+
+    }
 
     @Disabled
     @DisplayName("验收测试")
